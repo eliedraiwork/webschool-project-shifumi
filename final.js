f41e98d7eeff4ae8c0f58ce6bd629595ec9b2f75
Elie Drai <elie.drai.work@gmail.com>
2022-01-12 09:35:09 +0200

    Init the game

---
+++ final.js
@@ -0,0 +1,51 @@
+const SCISSORS_SIGN = '✂️';
+const LEAF_SIGN = '🍃';
+const STONE_SIGN = '🪨';
+
+/**
+ * This function return a boolean, according to the choice of the user:
+ * Continue the game    ==> true
+ * Leave the game       ==> false
+ */
+function continueTheGame() {
+	// TODO: implement this function
+}
+
+/**
+ * This function return one of the signs, according to user choice
+ * SCISSORS_SIGN = '✂️';
+ * LEAF_SIGN = '🍃';
+ * STONE_SIGN = '🪨';
+ */
+function chooseASign() {
+	// TODO: implement this function
+}
+
+/**
+ * This function return a number according to the comparison:
+ * sign1 wins over sign2 ==> return 1
+ * sign2 wins over sign1 ==> return -1
+ * sign1 and sign2 are equals ==> return 0
+ */
+function compareSigns(sign1, sign2) {
+	// TODO: implement this function
+}
+
+/**
+ * Make the computer choose randomly one of the signs
+ * Return the sign chosen by the computer (SCISSORS_SIGN, LEAF_SIGN, STONE_SIGN)
+ */
+function randomChooseSign() {
+	// TODO: implement this function
+}
+
+/**
+ * Run the program in a loop until the user asks to leave.
+ * Keep the scores during the current game and display the scores at the end of every game.
+ */
+function main() {
+	// TODO: implement this function
+}
+
+//  Run the program
+main();
